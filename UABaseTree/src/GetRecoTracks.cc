d9f65d97923fea7e56e0dcea6f04a301fefa029b
rougny <>
2011-02-14 17:52:31 +0000

    Added the function FillTrack , so that it can be reused (in Jets for instance).

--- UABaseTree/src/GetRecoTracks.cc
+++ UABaseTree/src/GetRecoTracks.cc
@@ -24,45 +24,7 @@ void UABaseTree::GetRecoTracks(const edm::Event& iEvent, const string TrackCollN
 
   for(TrackCollection::const_iterator tr = tracks->begin(); tr!=tracks->end(); ++tr){
 
-    mytrack.Reset();
-
-    mytrack.charge = tr->charge();
-    mytrack.v.SetPtEtaPhiM(tr->pt(),tr->eta(),tr->phi(), mpion );  
-
-    mytrack.nhit       =  tr->recHitsSize();
-    mytrack.npixhit    =  tr->hitPattern().pixelLayersWithMeasurement() ;
-    mytrack.nvalidhit  =  tr->found();
-    mytrack.chi2n      =  tr->normalizedChi2();
-    mytrack.dz         =  tr->dz();
-    mytrack.d0         =  tr->d0();
-    mytrack.edz        =  tr->dzError();
-    mytrack.ed0        =  tr->d0Error();
-    mytrack.ept        =  tr->ptError();
-
-    mytrack.vx    =  tr->vertex().x();
-    mytrack.vy    =  tr->vertex().y();
-    mytrack.vz    =  tr->vertex().z();
-
-    mytrack.quality[0] = tr->quality(reco::TrackBase::qualityByName("loose"));
-    mytrack.quality[1] = tr->quality(reco::TrackBase::qualityByName("tight"));
-    mytrack.quality[2] = tr->quality(reco::TrackBase::qualityByName("highPurity"));
-    mytrack.quality[3] = tr->quality(reco::TrackBase::qualityByName("confirmed"));
-    mytrack.quality[4] = tr->quality(reco::TrackBase::qualityByName("goodIterative"));
-
-    mytrack.trackAlgo = tr->algo();
-
-    // BeamSpot (id=0) and Vertex (id>0) Links
-    mytrack.vtxid.clear();
-    mytrack.vtxdxy.clear();
-    mytrack.vtxdz.clear();
-
-    for ( int i = 0 ; i != vtxid ; i++ ){
-       mytrack.vtxid.push_back( i ); 
-       mytrack.vtxdxy.push_back( tr->dxy( vtxid_xyz[i] ) );
-       mytrack.vtxdz.push_back(  tr->dz( vtxid_xyz[i] )  );
-    }
-
-
+    this->FillTrack(*tr , mytrack);
     TrackVector.push_back(mytrack);
     
     if(TrackDebug) mytrack.Print();
@@ -75,4 +37,47 @@ void UABaseTree::GetRecoTracks(const edm::Event& iEvent, const string TrackCollN
 void UABaseTree::GetAllTracks( const edm::Event& iEvent ){
   for(vector<InputTag>::iterator icoll = tracks_.begin() ; icoll!= tracks_.end() ; ++icoll)
     this->GetRecoTracks(iEvent , icoll->label() , allTracks[icoll->label()] );
+}
+
+
+
+void UABaseTree::FillTrack(const Track& intrack , MyTracks& outtrack){
+
+  outtrack.Reset();
+
+  outtrack.charge = intrack.charge();
+  outtrack.v.SetPtEtaPhiM(intrack.pt(),intrack.eta(),intrack.phi(), mpion );  
+
+  outtrack.nhit	   =  intrack.recHitsSize();
+  outtrack.npixhit    =  intrack.hitPattern().pixelLayersWithMeasurement() ;
+  outtrack.nvalidhit  =  intrack.found();
+  outtrack.chi2n	   =  intrack.normalizedChi2();
+  outtrack.dz	   =  intrack.dz();
+  outtrack.d0	   =  intrack.d0();
+  outtrack.edz	   =  intrack.dzError();
+  outtrack.ed0	   =  intrack.d0Error();
+  outtrack.ept	   =  intrack.ptError();
+
+  outtrack.vx    =  intrack.vertex().x();
+  outtrack.vy    =  intrack.vertex().y();
+  outtrack.vz    =  intrack.vertex().z();
+
+  outtrack.quality[0] = intrack.quality(TrackBase::qualityByName("loose"));
+  outtrack.quality[1] = intrack.quality(TrackBase::qualityByName("tight"));
+  outtrack.quality[2] = intrack.quality(TrackBase::qualityByName("highPurity"));
+  outtrack.quality[3] = intrack.quality(TrackBase::qualityByName("confirmed"));
+  outtrack.quality[4] = intrack.quality(TrackBase::qualityByName("goodIterative"));
+
+  outtrack.trackAlgo = intrack.algo();
+
+  // BeamSpot (id=0) and Vertex (id>0) Links
+  outtrack.vtxid.clear();
+  outtrack.vtxdxy.clear();
+  outtrack.vtxdz.clear();
+
+  for ( int i = 0 ; i != vtxid ; i++ ){
+    outtrack.vtxid.push_back( i ); 
+    outtrack.vtxdxy.push_back( intrack.dxy( vtxid_xyz[i] ) );
+    outtrack.vtxdz.push_back(  intrack.dz( vtxid_xyz[i] )  );
+  }
 }
