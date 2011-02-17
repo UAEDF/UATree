675c50dda4c153b09cfbfebb9652f51a92bd5d5d
rougny <>
2011-02-17 17:13:47 +0000

    added number of valid hits from subdetectors for higgs study

--- UADataFormat/src/MyTracks.h
+++ UADataFormat/src/MyTracks.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include "TObject.h"
 #include "MyPart.h"
+#include <iostream>
+
+using namespace std;
 
 class MyTracks : public MyPart {
 
@@ -16,7 +19,16 @@ class MyTracks : public MyPart {
 
     Bool_t   quality[5];
     Int_t    trackAlgo;
-    Int_t    nhit , nvalidhit , npixhit;
+    Int_t    nhit;
+    Int_t    nValidPixelHits;
+    Int_t    nValidStripHits;  //can also look individual layer but do it later if needed ...
+    Int_t    nValidMuonCSCHits;
+    Int_t    nValidMuonDTHits;
+    Int_t    nValidMuonRPCHits;
+    
+    Int_t    nValidTrackerHits(){return nValidPixelHits+nValidStripHits;                     };
+    Int_t    nValidMuonHits   (){return nValidMuonCSCHits+nValidMuonDTHits+nValidMuonRPCHits;};
+
     Double_t chi2n;
     Double_t dz  , d0 ;
     Double_t edz , ed0 , ept ; 
